7e5583191b54cfa6d5367efcd6fd42bacf2e5beb
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-22 13:00:01 +0800

    homework

--- src/main/java/ru/otus/java/homework/AppArrayDataException.java
+++ src/main/java/ru/otus/java/homework/AppArrayDataException.java
@@ -1,5 +1,7 @@
 package ru.otus.java.homework;
 
 public class AppArrayDataException extends RuntimeException{
-
+    public AppArrayDataException(String message) {
+        super(message);
+    }
 }
--- src/main/java/ru/otus/java/homework/AppArraySizeException.java
+++ src/main/java/ru/otus/java/homework/AppArraySizeException.java
@@ -1,6 +1,8 @@
 package ru.otus.java.homework;
 
 public class AppArraySizeException extends RuntimeException {
-
+    public AppArraySizeException(String message) {
+        super(message);
+    }
 }
 
